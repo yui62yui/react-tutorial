ee10ae17b5403a242be2e39b077d8d51a761637c
yui62yui <you@example.com>
2023-07-27 15:33:34 +0900

    redux toolkit 통해 기본 데이터 세팅 완료

--- src/index.js
+++ src/index.js
@@ -2,12 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice, nanoid } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+let datas = createSlice({
+  name: "datas",
+  initialState: [
+    {
+      id: nanoid(),
+      title: "이것은 제목입니다1",
+      content: "이것은 내용인뎁쇼1",
+      author: "111@naver.com",
+    },
+    {
+      id: nanoid(),
+      title: "이것은 제목입니다2",
+      content: "이것은 내용인뎁쇼2",
+      author: "222@naver.com",
+    },
+    {
+      id: nanoid(),
+      title: "이것은 제목입니다3",
+      content: "이것은 내용인뎁쇼3",
+      author: "333@naver.com",
+    },
+  ],
+});
+
 const store = configureStore({
-  reducer: counterSlice,
+  reducer: {
+    datas: datas.reducer,
+  },
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
